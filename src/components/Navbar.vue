e51b367aa02c3dbcb5daa9a85c5f33ac6e33ea16
Nick <fr13.dev@gmail.com>
2021-03-12 08:21:56 +0300

    improve navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,17 +2,22 @@
   <nav class="navbar navbar-expand-md navbar-light bg-light mb-1">
     <div class="navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto" v-if="workplace.id !== ''">
-        <li class="nav-item active mr-3">
-          <p>Workplace:</p>
-          <strong>{{ workplace.name }}</strong>
+        <li class="nav-item mr-3">
+          <div class="mx-3">
+            <h5>
+              Workplace: <strong>{{ workplace.name }}</strong>
+            </h5>
+          </div>
         </li>
-        <li class="nav-item active mr-3">
-          <p>Active orders:</p>
-          <strong>{{ activeOrdersNumber || 0 }}</strong>
+        <li class="nav-item mr-3">
+          <div>
+            <h5>
+              Active orders: <strong>{{ activeOrdersNumber || 0 }}</strong>
+            </h5>
+          </div>
         </li>
       </ul>
     </div>
-
   </nav>
 </template>
 
