82adec1aba99f3b86c729de0706b12b628a26fc2
Nick <fr13.dev@gmail.com>
2021-03-17 10:44:07 +0300

    add basic backend impl

---
+++ backend/src/main/java/dev/fr13/OrdersDisplay.java
@@ -0,0 +1,12 @@
+package dev.fr13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersDisplay {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersDisplay.class, args);
+    }
+}
