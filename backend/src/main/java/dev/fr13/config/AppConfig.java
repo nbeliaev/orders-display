8d08454be4986c81185770861f1dcebd356f6c18
Nick <fr13.dev@gmail.com>
2021-03-19 14:21:15 +0300

    split service workplace and repository

--- backend/src/main/java/dev/fr13/config/AppConfig.java
+++ backend/src/main/java/dev/fr13/config/AppConfig.java
@@ -2,18 +2,13 @@ package dev.fr13.config;
 
 import dev.fr13.domain.OrderItem;
 import dev.fr13.dtos.OrderItemDto;
-import dev.fr13.persistence.services.WorkplaceService;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-    @Autowired
-    private WorkplaceService workplaceService;
 
     @Bean
     public ModelMapper modelMapper() {
--- backend/src/main/java/dev/fr13/persistence/services/WorkplaceServiceImpl.java
+++ backend/src/main/java/dev/fr13/persistence/services/WorkplaceServiceImpl.java
@@ -2,48 +2,41 @@ package dev.fr13.persistence.services;
 
 import dev.fr13.domain.Workplace;
 import dev.fr13.dtos.WorkplaceDto;
+import dev.fr13.persistence.reps.WorkplaceRepository;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
-public class WorkplaceServiceInMemory implements WorkplaceService {
-    private static final Logger log = LoggerFactory.getLogger(WorkplaceServiceInMemory.class);
+public class WorkplaceServiceImpl implements WorkplaceService {
+    private static final Logger log = LoggerFactory.getLogger(WorkplaceServiceImpl.class);
 
-    private static final List<Workplace> workplaces = new ArrayList<>();
     private final ModelMapper mapper;
+    private final WorkplaceRepository repository;
 
-    static {
-        var kitchen = new Workplace("K", "Kitchen #1");
-        var bar = new Workplace("B", "Bar");
-        workplaces.add(kitchen);
-        workplaces.add(bar);
-    }
-
-    public WorkplaceServiceInMemory(ModelMapper mapper) {
+    public WorkplaceServiceImpl(ModelMapper mapper, WorkplaceRepository repository) {
         this.mapper = mapper;
+        this.repository = repository;
     }
 
     @Override
     public List<WorkplaceDto> findAllActive() {
         log.debug("Find all active workplaces");
+        var workplaces = repository.findAllActive();
         return workplaces.stream()
-                .filter(Workplace::isActive)
                 .map(i -> mapper.map(i, WorkplaceDto.class))
                 .collect(Collectors.toList());
     }
 
     @Override
     public Workplace findByUuid(String uuid) {
-        return workplaces.stream()
-                .filter(i -> i.getUuid().equals(uuid))
-                .findFirst()
+        var workplace = repository.findByUuid(uuid);
+        return workplace
                 .orElseThrow(() -> new NoSuchElementException(String.format("Workplace with uuid %s was not found.", uuid)));
     }
 
@@ -51,7 +44,7 @@ public class WorkplaceServiceInMemory implements WorkplaceService {
     public WorkplaceDto save(WorkplaceDto dto) {
         var workplace = mapper.map(dto, Workplace.class);
         log.debug("Save {}", workplace);
-        workplaces.add(workplace);
+        repository.save(workplace);
         return dto;
     }
 }
