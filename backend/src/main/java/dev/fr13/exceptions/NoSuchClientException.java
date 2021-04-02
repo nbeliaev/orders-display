c72f7d14cf161f59aa404f0a683b86511c6ad743
Nick <fr13.dev@gmail.com>
2021-04-02 13:43:58 +0300

    style

--- backend/src/main/java/dev/fr13/exceptions/NoSuchClientException.java
+++ backend/src/main/java/dev/fr13/exceptions/NoSuchClientException.java
@@ -2,7 +2,7 @@ package dev.fr13.exceptions;
 
 public class NoSuchClientException extends RuntimeException{
 
-    public NoSuchClientException(String workplaceUuid) {
-        super(String.format("Client with uuid %s was not found.", workplaceUuid));
+    public NoSuchClientException(String clientUuid) {
+        super(String.format("Client with uuid %s was not found.", clientUuid));
     }
 }
