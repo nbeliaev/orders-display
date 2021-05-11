553e64fa5034d5dae39c6263adbe903e7c90d5ce
Nick <fr13.dev@gmail.com>
2021-05-11 08:52:50 +0300

    remove program routing

--- frontend/src/components/Breadcrumbs.vue
+++ frontend/src/components/Breadcrumbs.vue
@@ -4,7 +4,6 @@
       <li class="breadcrumb-item fs-5"
           v-for="(breadcrumb, ind) in allBreadcrumbs"
           :key="ind"
-          @click="routeTo(ind)"
           :class="{active: !breadcrumb.link}">
         <a :href="breadcrumb.link">
           {{ breadcrumb.name }}
@@ -29,13 +28,7 @@ export default {
   },
   computed: mapGetters(['allBreadcrumbs']),
   methods: {
-    ...mapActions(['updateBreadcrumbs']),
-    routeTo(ind) {
-      const url = this.breadcrumbs[ind].link
-      if (url) {
-        this.$router.push(url)
-      }
-    }
+    ...mapActions(['updateBreadcrumbs'])
   }
 }
 </script>
